d0eaed9319d7eed7e136b1d98218ea892c9fb175
Aayushyadav2005 <aayushyadav94250@gmail.com>
2026-06-18 12:41:34 +0530

    analyzer works now

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,19 @@
-import * as vscode from 'vscode';
-import { createProject } from './commands/createProject';
+import * as vscode from "vscode";
+import { createProject } from "./commands/createProject";
+import { analyzeComplexity } from "./analyzers/complexityAnalyzer";
 
 export function activate(context: vscode.ExtensionContext) {
-    const disposable = vscode.commands.registerCommand(
-        'project-wizard.createProject',
-        createProject
-    );
+  const disposable = vscode.commands.registerCommand(
+    "project-wizard.createProject",
+    createProject,
+  );
 
-    context.subscriptions.push(disposable);
+  const complexityDisposable = vscode.commands.registerCommand(
+    "project-wizard.analyzeComplexity",
+    analyzeComplexity,
+  );
+
+  context.subscriptions.push(disposable, complexityDisposable);
 }
 
 export function deactivate() {}
